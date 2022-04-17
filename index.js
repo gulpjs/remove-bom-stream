281535c79a65ad1020a35305bf8445644edaaa8f
sttk <sttk.xslet@gmail.com>
2022-04-17 14:17:03 +0900

    fix: treat a case that a BOM is not removed on Node <= v11

--- index.js
+++ index.js
@@ -3,9 +3,15 @@
 var through = require('through2');
 var TextDecoder = require('util').TextDecoder;
 
+var BOM = Buffer.from([0xEF, 0xBB, 0xBF], 'utf-8');
+
 function removeBomStream(encoding) {
   encoding = (encoding || '').toLowerCase();
   var isUTF8 = (encoding === 'utf-8' || encoding === 'utf8');
+
+  if (!isUTF8) {
+    return through();
+  }
 
   // Only used if encoding is UTF-8
   var decoder = new TextDecoder('utf-8', { ignoreBOM: false });
@@ -15,23 +21,33 @@ function removeBomStream(encoding) {
   return through(onChunk);
 
   function onChunk(data, _, cb) {
-    if (state === 1 || !isUTF8) {
+    if (state === 1) {
       cb(null, data);
-    } else {
-      try {
-        state = -1;
+      return;
+    }
 
-        var chunk = decoder.decode(data, { stream: true });
+    try {
+      state = -1;
 
-        // The first time we have data after a decode, it should have already removed the BOM
-        if (chunk !== '') {
-          state = 1
+      var chunk = decoder.decode(data, { stream: true });
+
+      // The first time we have data after a decode, it should have already removed the BOM
+      if (chunk !== '') {
+        chunk += decoder.decode(); // end of stream mode and clear inner buffer.
+
+        var buffer = Buffer.from(chunk, 'utf-8');
+
+        // Node<=v11, TextDecoder#decode returns a BOM if it receives a BOM separately.
+        if (BOM.compare(buffer) !== 0) {
+          state = 1;
+          cb(null, buffer);
+          return;
         }
-
-        cb(null, Buffer.from(chunk, encoding));
-      } catch (err) {
-        cb(err);
       }
+
+      cb();
+    } catch (err) {
+      cb(err);
     }
   }
 }
